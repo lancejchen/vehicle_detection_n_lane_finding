485d64bb58fc940e41da43f5eec720f30b9f01ab
lanceturnbull <lancejchen@gmail.com>
2017-02-11 16:36:44 +0800

    AWS test

--- src/VehicleDetectionHeatMap.py
+++ src/VehicleDetectionHeatMap.py
@@ -1,7 +1,19 @@
 from collections import deque
+import numpy as np
 
-
-class VehicleDetectionHeatmap:
+class VehicleDetectionHeatMap:
     def __init__(self):
-        recent_heatmaps = deque(maxlen=5)
-        combined_heatmap
+        self.recent_heatmaps = deque(maxlen=5)
+        self.combined_heatmap = None
+
+    def combine_recent_heatmap(self, new_heatmap):
+        self.recent_heatmaps.append(new_heatmap)
+        self.combined_heatmap = np.zeros_like(new_heatmap)
+        # insert new heap map to deque
+        for array in self.recent_heatmaps:
+            self.combined_heatmap += array
+        return self.combined_heatmap
+
+
+
+
